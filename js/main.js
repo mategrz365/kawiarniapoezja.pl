310d3df24f2c45637b91db395418dbfefbd0db39
mategrz365 <mategrz365@gmail.com>
2018-04-05 19:38:12 +0200

    up

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 
+
 $(document).ready(function() {
-    $('.parallax-window').parallax({imageSrc: 'img/parallax.jpg'});
-    });
-    /*var scroll = function(offset) {
+   
+    var scroll = function(offset) {
         $('html,body').animate({scrollTop: offset}, 500);
     }
+    
+    
 
     $(".hamburger").click(function() {
         $(".page-nav").stop(true, true).fadeToggle();
@@ -19,16 +21,17 @@ $(document).ready(function() {
    $(".arrow").click(function() {
         scroll(0);
     });
-*/
 
-document.addEventListener("DOMContentLoaded", function(){
+
+
+/*document.addEventListener("DOMContentLoaded", function(){
     
-/*const btn = document.querySelector(".button");
+const btn = document.querySelector(".button");
 const menu = document.querySelector(".menu-select");
     
-btn.addEventListener('click',() => menu.style.display="block")*/
+btn.addEventListener('click',() => menu.style.display="block")
     
-/*     
+     
 const list = document.querySelector(".menu-rest");
 const btns1 = list.querySelectorAll('.menu-rest-select');
 const cont = list.querySelectorAll('.menu-rest-module');   
@@ -36,6 +39,8 @@ const cont = list.querySelectorAll('.menu-rest-module');
     for(let i=0; i<btns1.length;i++){
          btns1[i].addEventListener('click', function(){
                 for (let i =0;i<cont.length;i++){
+             
+             
              
             cont[i].classList.remove('show');
             cont[i].dataset.show = 0;
@@ -56,8 +61,8 @@ const cont = list.querySelectorAll('.menu-rest-module');
 });
 
 
-      
-    */
+     */ 
+    
 
 
    
