d8f9ec77e22b2e84cd5b6ac225e2a10c134834d6
Bochenek, Lukasz <lukasz.bochenek@sap.com>
2018-04-05 21:36:10 +0200

    Restaurant menu navigation improved

--- js/main.js
+++ js/main.js
@@ -18,68 +18,16 @@ $(document).ready(function() {
         scroll($(this.hash).offset().top);
     });
 
-   $(".arrow").click(function() {
+    $(".arrow").click(function() {
         scroll(0);
     });
 
+    $(".menu-rest-select").click(function() {
+        var $self = $(this);
+        var index = $self.data("index");
+        scroll($self.position().top - 100);
 
-
-/*document.addEventListener("DOMContentLoaded", function(){
-    
-const btn = document.querySelector(".button");
-const menu = document.querySelector(".menu-select");
-    
-btn.addEventListener('click',() => menu.style.display="block")
-    
-     
-const list = document.querySelector(".menu-rest");
-const btns1 = list.querySelectorAll('.menu-rest-select');
-const cont = list.querySelectorAll('.menu-rest-module');   
-   
-    for(let i=0; i<btns1.length;i++){
-         btns1[i].addEventListener('click', function(){
-                for (let i =0;i<cont.length;i++){
-             
-             
-             
-            cont[i].classList.remove('show');
-            cont[i].dataset.show = 0;
-            btns1[i].style.background="none";
-        
-            cont[i].classList.add('show');
-            cont[i].dataset.show = 1; 
-            btns1[i].style.background="#c59d5f";
-            btns1[i].style.color="#555"; 
-                }
-             
-             
-         });
-    }
-        
-         
-    
+        $(".menu-rest-module").hide();
+        $(".menu-rest-module[data-index='" + index + "']").fadeIn();
+    });
 });
-
-
-     */ 
-    
-
-
-   
-        
-      
-
-    
-
-    
-   
-      
-        
-      
-    
-        
-   
-  
-
-
-
