2b4b496e4764b9adf4dd68e9916faf1d4c49b30b
mategrz365 <mategrz365@gmail.com>
2018-04-15 09:49:22 +0200

    js changed
    restaurant menu added.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function() {
    
     var scroll = function(offset) {
@@ -22,51 +21,19 @@ $(document).ready(function() {
         scroll(0);
     });
 
-
-
-/*document.addEventListener("DOMContentLoaded", function(){
     
-const btn = document.querySelector(".button");
-const menu = document.querySelector(".menu-select");
+    $(".menu-rest-select").click(function(){
+        $index = $(this).data('index');
+        $('.menu-rest-module').hide();/*.removeClass('active');*/
+        $('.menu-rest-module[data-index='+$index+']').fadeIn(500);/*addClass('active'); */       
+    });
     
-btn.addEventListener('click',() => menu.style.display="block")
-    
-     
-const list = document.querySelector(".menu-rest");
-const btns1 = list.querySelectorAll('.menu-rest-select');
-const cont = list.querySelectorAll('.menu-rest-module');   
-   
-    for(let i=0; i<btns1.length;i++){
-         btns1[i].addEventListener('click', function(){
-                for (let i =0;i<cont.length;i++){
-             
-             
-             
-            cont[i].classList.remove('show');
-            cont[i].dataset.show = 0;
-            btns1[i].style.background="none";
-        
-            cont[i].classList.add('show');
-            cont[i].dataset.show = 1; 
-            btns1[i].style.background="#c59d5f";
-            btns1[i].style.color="#555"; 
-                }
-             
-             
-         });
-    }
-        
-         
     
 });
-
-
-     */ 
     
-
-
-   
-        
+    
+    
+ 
       
 
     
